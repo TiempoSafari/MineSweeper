4aab63648a0a7f1a298b5ed270cc6739af9abda1
TiempoSafari <13936525918@163.com>
2026-01-03 17:44:25 +0800

    Initial Commit

---
+++ MineSweeperTests/MineSweeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MineSweeperTests.swift
+//  MineSweeperTests
+//
+//  Created by 山枫 on 2026/1/3.
+//
+
+import Testing
+@testable import MineSweeper
+
+struct MineSweeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
